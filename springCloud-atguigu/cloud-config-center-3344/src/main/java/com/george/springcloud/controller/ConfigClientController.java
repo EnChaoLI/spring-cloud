954cfb860b83cc13e4e83dd1d01d57092c61dab8
enchaolee <1159686526@qq.com>
2020-11-11 15:52:13 +0800

    init project

---
+++ springCloud-atguigu/cloud-config-center-3344/src/main/java/com/george/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.george.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author enchaolee
+ */
+@RestController
+@Slf4j
+public class ConfigClientController {
+
+
+    @GetMapping("/configInfo")
+    public void getConfigInfo() {
+        System.out.println("==============github push 回调测试成功===========");
+    }
+
+}
