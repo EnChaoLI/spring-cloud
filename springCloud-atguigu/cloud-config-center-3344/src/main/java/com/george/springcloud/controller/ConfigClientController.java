75a8db8f402a053505ece7c4e53d430355036e9f
enchaolee <1159686526@qq.com>
2020-11-11 15:53:27 +0800

    init project

--- springCloud-atguigu/cloud-config-center-3344/src/main/java/com/george/springcloud/controller/ConfigClientController.java
+++ springCloud-atguigu/cloud-config-center-3344/src/main/java/com/george/springcloud/controller/ConfigClientController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConfigClientController {
 
 
-    @GetMapping("/configInfo")
+    @PostMapping("/configInfo")
     public void getConfigInfo() {
         System.out.println("==============github push 回调测试成功===========");
     }
